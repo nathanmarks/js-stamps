87c914d9c6d27beb94b05955ad89b7d40996e080
Nathan Marks <nathan@poweredbysearch.com>
2016-01-15 11:07:52 -0500

    Fix import paths

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
  * Registers the babel runtime transpilation
  * and loads up the library
  */
-require('babel.config');
-module.exports = require('lib');
+require('./babel.config');
+module.exports = require('./lib');
